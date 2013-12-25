861fe083f9e65edd820b8a8d5ed44e594a3cbdfc
José Carlos Baquero <jbaquerot@gmail.com>
2013-12-25 23:42:39 +0100

    part 3 - uncompleted

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,11 @@
-from django.http import Http404
-from django.shortcuts import render
-from polls.models import Poll
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from polls.models import Poll, Choice
 
 # Create your views here.
 def index(request):
-	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
 	context = {'latest_poll_list': latest_poll_list}
 	return render(request, 'polls/index.html', context)
 
@@ -16,4 +17,16 @@ def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-	return HttpResponse("You're voting on poll %s." % poll_id)
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the poll voting form.
+		return render(request, 'polls/detail.html', {
+			'poll': p,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('poll:results', args=(p.id,)))
